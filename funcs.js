361a2ced2c5f34aac99ec63f2bcaca7441b34f9c
hrj2233 <hrj2233@gmail.com>
2022-05-31 13:16:59 +0900

    practice mocha and chai

--- funcs.js
+++ funcs.js
@@ -1,14 +1,22 @@
 function isFive(num) {
   // Your code here
+  return num === 5 ? true : false;
 }
 
 function isOdd(number) {
   // Your code here
+  if (typeof number !== "number") {
+    throw new Error("this is not a number");
+  }
+  return number % 2 !== 0 ? true : false;
 }
 
 function myRange(min, max, step = 1) {
   // Your code here
+  let newArr = [];
+  for (let i = min; i <= max; i += step) {
+    newArr.push(i);
+  }
+  return newArr;
 }
-
-
 module.exports = { isFive, isOdd, myRange };
--- test/class-spec.js
+++ test/class-spec.js
@@ -1,34 +1,43 @@
 const chai = require("chai");
+const { userInfo } = require("os");
 const expect = chai.expect;
 
 const { Word } = require("../class");
 
 describe("Word", function () {
+  let word;
+  beforeEach(() => {
+    word = new Word("test");
+  });
+
   describe("Word constructor function", function () {
     it('should have a "word" property', function () {
-      expect.fail("replace with your code");
+      expect(word).to.exist;
     });
-  
+
     it('should set the "word" property when a new word is created', function () {
-      expect.fail("replace with your code");
+      expect(word).to.exist;
     });
   });
 
   describe("removeVowels function", function () {
     it("should return a the word with all vowels removed", function () {
-      expect.fail("replace with your code");
+      let test2 = word.removeVowels();
+      expect(test2).to.equal("tst");
     });
   });
 
   describe("removeConsonants function", function () {
     it("should return the word with the consonants removed", function () {
-      expect.fail("replace with your code");
+      let test3 = word.removeConsonants();
+      expect(test3).to.equal("e");
     });
   });
-  
+
   describe("pigLatin function", function () {
     it("should return the word converted to pig latin", function () {
-      expect.fail("replace with your code");
+      let test4 = word.pigLatin();
+      expect(test4).to.equal("esttay");
     });
   });
 });
